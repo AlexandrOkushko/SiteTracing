1dd727d9ed6f425dc1867a327c75bbd9ef4ca48f
AlexandrOkushko <sevenr23@gmail.com>
2021-01-05 00:26:10 +0200

    Added a view of HttpGet method 'Create'.

--- Models/Data/Db.cs
+++ Models/Data/Db.cs
@@ -9,5 +9,7 @@ namespace SiteTracing.Models.Data
         public DbSet<TraceDetailsDTO> TraceDetails { get; set; }
 
         public DbSet<SearchDTO> Searches { get; set; }
+
+        public System.Data.Entity.DbSet<SiteTracing.Models.ViewModels.SearchVM> SearchVMs { get; set; }
     }
 }
